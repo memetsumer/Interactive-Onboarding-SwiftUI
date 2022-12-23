94058cc03fc424dfd4f0e0a191190ef8efb3a55c
Mehmet <sumer682000@gmail.com>
2022-12-23 13:57:11 +0300

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Mehmet Sümer on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
